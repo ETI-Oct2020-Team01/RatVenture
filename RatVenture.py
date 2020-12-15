d45a4bef117c65411ccb909e2f098b0210948b6d
Irfan Syahmi Mohd Isa <s10189620@connect.np.edu.sg>
2020-12-15 19:55:46 +0800

    Barebones main menu (test commit/push to own branch)

--- RatVenture.py
+++ RatVenture.py
@@ -1,3 +1,38 @@
-#RatVenture
-#Description: Computer role-playing game(RPG) called RatVenture which is text-based.
-#             Your objective is to defeat the Rat King and save the world!.
+#####################
+##### RatVenture ####
+#####################
+
+### Imports ###
+import sys
+
+
+### Main Menu ###
+def mainMenu():
+    print("Welcome adventurer, to RatVenture!")
+    print("----------------------------------")
+    print("Please select an option:")
+    print("1) New Game")
+    print("2) Resume Game")
+    print("3) Exit Game")
+
+mainMenu()
+### Main Menu Options ###
+#def mainMenu_options():
+option = int(input("Enter your option: "))
+
+while option != 0:
+    if option == 1:
+        # do option 1 stuff
+        print("Option 1 has been called")
+    elif option == 2:
+        # do option 2 stuff
+        print("Option 2 has been called")
+    elif option == 3: 
+        # do option 3 stuff
+        print("Option 3 has been called")
+    else:
+        print("Invalid option. Please choose between the three given options.")
+
+    print()
+    mainMenu()
+    option = int(input("Enter your option: "))
