1e336c199f0116f886299b535d2f9ffd68277bd1
nurinnazurah <S10189240@connect.np.edu.sg>
2020-12-16 12:52:54 +0800

    added of the default world map

--- RatVenture.py
+++ RatVenture.py
@@ -1,3 +1,17 @@
 #RatVenture
 #Description: Computer role-playing game(RPG) called RatVenture which is text-based.
 #             Your objective is to defeat the Rat King and save the world!.
+
+#setting of global variables
+global world_map
+
+#Default world map
+world_map = [['T', ' ', ' ', ' ', ' ', ' ', ' ', ' '],\
+             [' ', ' ', ' ', 'T', ' ', ' ', ' ', ' '],\
+             [' ', ' ', ' ', ' ', ' ', 'T', ' ', ' '],\
+             [' ', 'T', ' ', ' ', ' ', ' ', ' ', ' '],\
+             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],\
+             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],\
+             [' ', ' ', ' ', ' ', 'T', ' ', ' ', ' '],\
+             [' ', ' ', ' ', ' ', ' ', ' ', ' ', 'K']]
+
