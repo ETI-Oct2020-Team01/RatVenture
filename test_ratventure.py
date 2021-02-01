97a548b0c7af60fb011040c39d81d7f6c9aabd35
nurinnazurah <S10189240@connect.np.edu.sg>
2021-02-01 22:12:23 +0800

    update pytest functions

--- test_ratventure.py
+++ test_ratventure.py
@@ -1,26 +1,41 @@
 # Test script for RatVenture's functions & features
 
 import pytest
+import random
 from RatVenture import *
 
+player = Player()
 
-# def testMainMenu(monkeypatch):
+def test_MainMenu(monkeypatch):
+    """
+    Input: 1
 
-#     monkeypatch.setattr('builtins.input', lambda _: 1)
-#     value = show_menu()
-#     assert value == 1
+    Output: 1
+    """
+    
+    monkeypatch.setattr("builtins.input", lambda _: 1)  
+    value = main_menu(player)
+    assert value == 1
 
-# def testMainMenu_over(monkeypatch):
-#     monkeypatch.setattr('builtins.input', lambda _: 6)
-#     value = show_menu(menu)
-#     assert value == "Invalid Input"
+def test_MainMenu_over(monkeypatch):
+    """
+    Input: 6
+    Output: Invalid choice
+    """
+    monkeypatch.setattr("builtins.input", lambda _: 6)  
+    value = main_menu(player)
+    assert value == "Invalid choice"
 
+def test_MainMenu_invalid(monkeypatch):
+    """
+    Input: a
+    Output: Invalid option. Enter only integers!
+    """
+    monkeypatch.setattr("builtins.input", lambda _: 9)
+    value = main_menu(player)
+    assert value == "Invalid option. Enter only integers!"
 
-def testFindEvent(find_event, event):
-    value = find_event()
-    assert value == event
-
-#@pytest.mark.parameterize('Town')
-def testItem(generate, item):
-    value = generate('Town')
-    assert value == item
+def test_rest_function():
+   """This is to test if rest function is working. By calling this function, player health will restore to 20 and add 1 day to the day count."""
+   value = rest()
+   assert value == (20, player.day)
