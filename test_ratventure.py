92960f70cc6b560583e8d1e0f957be845f8b6b16
nurinnazurah <S10189240@connect.np.edu.sg>
2021-02-10 21:39:57 +0800

    added new pytest

--- test_ratventure.py
+++ test_ratventure.py
@@ -17,3 +17,20 @@ def test_view_stats():
    value = herostats()
 
    assert value == "\nName: {} \nDamage: {}\nDefence: {}\nHP: {}\nDay: {}".format(player.name, player.damage, player.defence, player.hp, player.day)
+
+# test for rest function
+def test_rest_function():
+   """This is to test if rest function is working. The function will return the player health to 20 and will add a day to the player"""
+   value = rest()
+   assert value == (player.day)
+
+# test for invalid input of main menu
+def test_mainmenu_invalid(monkeypatch):
+    """
+    Input: 9
+
+    Output: Invalid choice 
+    """
+    monkeypatch.setattr("builtins.input", lambda _: 9)  
+    value = mainmenu_userinput()
+    assert value == "Invalid choice!"
