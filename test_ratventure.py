c9f1effdcf74c8f734d1f69def6f4ee1327b4ada
irfan-np <52643079+irfan-np@users.noreply.github.com>
2021-02-13 17:41:47 +0800

    Update test_ratventure.py
    added test for ratking stats for pytest

--- test_ratventure.py
+++ test_ratventure.py
@@ -10,27 +10,29 @@ def test_attack_function():
        value = attack()
        assert rat.hp < 10
 
-# test for hero stats function
+# test for displaying hero stats function
 def test_view_stats():
    """This is to test if the Hero Stats is displayed accordingly."""
-
    value = herostats()
-
    assert value == "\nName: {} \nDamage: {}\nDefence: {}\nHP: {}\nDay: {}".format(player.name, player.damage, player.defence, player.hp, player.day)
 
+# test for rat stats function
+def test_view_rat_stats():
+   """This is to test if the Rat Stats is displayed accordingly."""
+   value = ratstats()
+   assert value == "\nDamage: {}\nDefence: {}\nHP: {}".format(rat.damage, rat.defence, rat.hp)
+
+# test for displaying rat king stats function
+def test_ratking_stats():
+       """This is to test if the Rat King stats are correctly displayed. """
+       value = ratkingstats()
+       assert value == "\nDamage: {}\nDefence: {}\nHP: {}".format(rat_king.damage, rat_king.defence, rat_king.hp)
+       
 # test for rest function
 def test_rest_function():
    """This is to test if rest function is working. The function will return the player health to 20 and will add a day to the player"""
    value = rest()
    assert value == (player.day)
-
-# test for rat stats function
-def test_view_rat_stats():
-   """This is to test if the Rat Stats is displayed accordingly."""
-
-   value = ratstats()
-
-   assert value == "\nDamage: {}\nDefence: {}\nHP: {}".format(rat.damage, rat.defence, rat.hp)
 
 #test for run function
 def test_run_function(monkeypatch):
